7031909327095bc820e1bad228480fe57fac2dc9
CorentinGuerain <coguerain@stpbb.org>
2022-01-15 22:00:04 +0100

    include data + index de base

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+    include_once('inc.data.php');
+    include_once('inc.header.php');
+?>
+<html>
+    <head>
+        <title><?php echo $title?> Accueil</title>
+    </head>
+    <?php
+        include_once('inc.footer.php');
+    ?>
+</html>
