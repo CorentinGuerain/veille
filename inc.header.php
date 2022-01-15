18a7b9fd8d480e81d465ce4d46ce39151e170648
CorentinGuerain <coguerain@stpbb.org>
2022-01-15 21:55:52 +0100

    include header

---
+++ inc.header.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <link href="css/header.css" rel="stylesheet">
+    </head>
+    <div class="div_header">
+        <header>
+            <img src="img/django_header.png">
+        </header>
+    
+        <nav class="navbar_header">
+            <ul>
+                <li ><a href="/" id="home_a">Accueil</a></li>
+                <li><a href="#Apropos">Django</a></li>
+            </ul>
+        </nav>
+    </div>
+    
+</html>
